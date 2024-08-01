74425c3e456f8c84d037f185802b339a1767d0cc
Aman Pathak <85124250+AmanPathak-DevOps@users.noreply.github.com>
2024-08-01 21:43:11 +0530

    Added new features, animations v1.1.0

--- static/js/script.js
+++ static/js/script.js
@@ -1,23 +1,22 @@
+// script.js
+
 function showSection(sectionId) {
   const sections = document.querySelectorAll('section');
-  const navLinks = document.querySelectorAll('nav ul li a');
   sections.forEach(section => {
       section.classList.remove('active');
   });
-  document.getElementById(sectionId).classList.add('active');
   
+  document.getElementById(sectionId).classList.add('active');
+
+  const navLinks = document.querySelectorAll('.nav-link');
   navLinks.forEach(link => {
       link.classList.remove('active');
-      if (link.getAttribute('href') === `#${sectionId}`) {
-          link.classList.add('active');
-      }
   });
+
+  document.querySelector(`a[href="#${sectionId}"]`).classList.add('active');
 }
 
-// Show the section based on the hash in the URL when the page loads
-document.addEventListener('DOMContentLoaded', (event) => {
-  if (window.location.hash) {
-      const sectionId = window.location.hash.substring(1);
-      showSection(sectionId);
-  }
+// Show the home section by default
+document.addEventListener('DOMContentLoaded', () => {
+  showSection('home');
 });
